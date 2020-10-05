af8be50442542327b566f9289698b965017391b7
Ritesh16 <ritesh.sharma.cse@gmail.com>
2020-10-05 17:42:50 -0400

    Updates

---
+++ precache-manifest.dbe7ab204fe310b8c2c58ed82d2e2f2a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "332c7c71994bcfc7d432b51c38533cac",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "f57efe6d918256578995",
+    "url": "/weatherapp/static/css/2.38561f66.chunk.css"
+  },
+  {
+    "revision": "5485fe25b31be46d6c06",
+    "url": "/weatherapp/static/css/main.d5b9631c.chunk.css"
+  },
+  {
+    "revision": "f57efe6d918256578995",
+    "url": "/weatherapp/static/js/2.66c430ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherapp/static/js/2.66c430ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5485fe25b31be46d6c06",
+    "url": "/weatherapp/static/js/main.ed155402.chunk.js"
+  },
+  {
+    "revision": "8862584b26a768bb4b89",
+    "url": "/weatherapp/static/js/runtime-main.cb4f0870.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/weatherapp/static/media/SEGA.12a3db8b.woff"
+  }
+]);
